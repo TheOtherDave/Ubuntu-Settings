a70e80b7fe89dbd4f29d6a7a4a5a499ea5d853a9
Dave Sweeris <davesweeris@mac.com>
2019-06-21 09:16:21 -0700

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
 
+#General Settings
 gsettings set org.gnome.desktop.interface clock-show-seconds true
+
+#Stuff for Swift (beyond what its docs say to install)
+#In Ubuntu 19.xx, not the 18.xx LTS version that's supported
+sudo apt install libtinfo5
+sudo apt install libncurses5
